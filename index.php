5926d87577277d6148499f219fce01d4e69fce1e
Be Fernandes <fernandes252005@gmail.com>
2022-05-09 18:22:16 -0300

    Ite Help 0.1

---
+++ index.php
@@ -0,0 +1,339 @@
+<!Doctype html>
+<html lang="pt-br">
+
+    <head>
+	<title>ITE.HElP</title>
+        <meta charset="UTF-8">
+        <meta nome="viemport" content="width=device-width. initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <link  rel="shortcut icon" href="./logo.png" type="image/x-icon">
+    </head>
+
+
+    <body>
+		<header class="header">
+		   <a href="./index.php">Ajuda</a>
+           
+            <nav>
+                <ul class="menu">
+                    <li><a href="./sobre.php">Sobre</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <center><h1>Ajuda</h1></center>
+        <section class="flex">
+          
+			<!--<div>
+				<center><img src="logo.png"  width="75%"></center>
+			
+			</div>!-->
+             <div>
+				
+			 <form name="form1" method="post" border="1">
+				<fieldset>
+				<legend>Prencha o formulário para pedir ajuda</legend><br>
+
+					Nome Professor:<input type="text" name="prof">
+                    
+					<select name="professor">
+
+							<optgroup label="Escolha o nome do professor do bloco 2">
+							<option value="Adriana Marli Bastos Costa" label="Adriana Marli Bastos Costa"></option>
+							<option value="Adriano Fabri" label="Adriano Fabri"></option>
+							<option value="Alessa Berretini" label="Alessa Berretini"></option>
+							<option value="Alexandre Luiz Fantin Carreira" label="Alexandre Luiz Fantin Carreira"></option>
+							<option value="Ana Cecília Pegoraro Dias Masson" label="Ana Cecília Pegoraro Dias Masson"></option>
+							<option value="Ana Cristina de Carvalho Sales Toledo" label="Ana Cristina de Carvalho Sales Toledo"></option>
+							<option value="Anderson Talon" label="Anderson Talon"></option>
+                            <option value="André Murilo Parente Nogueira" label="André Murilo Parente Nogueira"></option>
+							<option value="Antonio Carlos Batista Martinez" label="Antonio Carlos Batista Martinez"></option>
+							<option value="Audrey do Nascimento Sabbatini Martins" label="Audrey do Nascimento Sabbatini Martins"></option>
+							<option value="Aurora Cannone" label="Aurora Cannone"></option>
+							<option value="Bruno Henrique Lucon" label="Bruno Henrique Lucon"></option>
+							<option value="Camila Roberta Muniz Serra" label="Camila Roberta Muniz Serra"></option>
+                            <option value="Carlos Eduardo Imaizumi" label="Carlos Eduardo Imaizumi"></option>
+							<option value="Carmen Francisca Lourenço Pinto Hellmeister" label="Carmen Francisca Lourenço Pinto Hellmeister"></option>
+							<option value="Cassiana Anunciata Caglioni" label="Cassiana Anunciata Caglioni"></option>
+							<option value="Cassio Passanezi Pegaroro" label="Cassio Passanezi Pegaroro"></option>
+                            <option value="Christiano César Martinello" label="Christiano César Martinello"></option>
+							<option value="Cláudio José Amaral Bahia" label="Cláudio José Amaral Bahia"></option>
+                            <option value="Cristiano Pansanato" label="Cristiano Pansanato"></option>
+							<option value="Daniel Lima Vialôgo" label="Daniel Lima Vialôgo"></option>
+							<option value="Daniela Nunes Veríssimo Gimenes" label="Daniela Nunes Veríssimo Gimenes"></option>
+                            <option value="Dércio Júlio Terrabuio Júnior" label="Dércio Júlio Terrabuio Júnior"></option>
+							<option value="Edison dos Santos Pelegrini" label="Edison dos Santos Pelegrini"></option>
+                            <option value="Edson Kiyotaka Yeiri Mitsuya" label="Edson Kiyotaka Yeiri Mitsuya">
+                            </option>
+                            <option value="Eduardo José Pereira Martin" label="Eduardo José Pereira Martin"></option>
+							<option value="Elis Angela dos Anjos" label="Elis Angela dos Anjos"></option>
+							<option value="Ellen Carina Mattias Sartori" label="Ellen Carina Mattias Sartori"></option>
+                            <option value="Ellen Francine de Oliveira Rossetto" label="Ellen Francine de Oliveira Rossetto"></option>
+							<option value="Eugênia Maria Sellmann Chaves" label="Eugênia Maria Sellmann Chaves"></option>
+							<option value="Erica Marcelina Cruz" label="Erica Marcelina Cruz"></option>
+                            <option value="Everson Demarchi" label="Everson Demarchi"></option>
+							<option value="Fábio Alexandre Coelho" label="Fábio Alexandre Coelho"></option>
+                            <option value="Fábio José Esguícero" label="Fábio José Esguícero"></option>
+							<option value="Flávio Mangili Ferreira" label="Flávio Mangili Ferreira"></option>
+							<option value="Francisco José Lampkowski" label="Francisco José Lampkowski"></option>
+                            <option value="Gerceley Paccola Minetto" label="Gerceley Paccola Minetto"></option>
+							<option value="Gilberto Vieira" label="Gilberto Vieira"></option>
+							<option value="Glauber Ricardo" label="Glauber Ricardo"></option>
+							<option value="Gustavo César Bruschi" label="Gustavo César Bruschi"></option>
+							<option value="Hideyuki Kashio" label="Hideyuki Kashio"></option>
+                            <option value="Igor Alberto Barbosa Turto" label="Igor Alberto Barbosa Turto"></option>
+							<option value="Ilda Chicaré Atauri" label="Ilda Chicaré Atauri"></option>
+							<option value="Ivone Aparecida Lucas" label="Ivone Aparecida Lucas"></option>
+							<option value="João Paulo Capiboanco Felipe" label="João Paulo Capiboanco Felipe"></option>
+							<option value="João Henrique Ferreira" label="João Henrique Ferreira"></option>
+                            <option value="José Augusto de Carvalho Dias" label="José Augusto de Carvalho Dias"></option>
+                            <option value="José Claudio Domingues Moreira" label="José Claudio Domingues Moreira"></option>
+                            <option value="José Munhoz Fernandes" label="José Munhoz Fernandes"></option>
+							<option value="José Otavio de Almeida Barros Junior" label="José Otavio de Almeida Barros Junior"></option>
+                            <option value="José Ricardo Scareli Carrijo" label="José Ricardo Scareli Carrijo"></option>
+							<option value="José Roberto Anselmo" label="José Roberto Anselmo"></option>
+							<option value="Josiane Fernandes Lozigia Carrapato" label="Josiane Fernandes Lozigia Carrapato"></option>
+							<option value="Juliana Maria Corvino de Araujo" label="Juliana Maria Corvino de Araujo"></option>
+                            <option value="Júlio César Marin de Carmo" label="Júlio César Marin de Carmo"></option>
+                            <option value="Leandro Ebúrneo Laposta" label="Leandro Ebúrneo Laposta"></option>
+                            <option value="Lilia Christina de Oliveira" label="Lilia Christina de Oliveira"></option>
+                            <option value="Luiz Bertonha Júnior" label="Luiz Bertonha Júnior"></option>
+							<option value="Luiz Carlos Gonçalves Filho" label="Luiz Carlos Gonçalves Filho"></option>
+                            <option value="Luiz Gino Farina de Oliveira" label="Luiz Gino Farina de Oliveira"></option>
+							<option value="Luiz Nunes Pegararo" label="Luiz Nunes Pegararo"></option>
+                            <option value="Luiz Otávio Marestoni Camalionte" label="Luiz Otávio Marestoni Camalionte"></option>
+							<option value="Magna Gabriella Vigano Cavalcanti" label="Magna Gabriella Vigano Cavalcanti"></option>
+							<option value="Manoel de Queiróz Pereira Calças" label="Manoel de Queiróz Pereira Calças"></option>
+                            <option value="Marcelo José Storion" label="Marcelo José Storion"></option>
+                            <option value="Marcos Vinício Bilancieri" label="Marcos Vinício Bilancieri"></option>
+                            <option value="Márcio Galvadão" label="Márcio Galvadão"></option>
+							<option value="Maria Cristina Lourenço dos Santos" label="Maria Cristina Lourenço dos Santos"></option>
+							<option value="Maria Inês Fontana" label="Maria Inês Fontana"></option>
+							<option value="Marinez Cristina Vitoreli" label="Marinez Cristina Vitoreli"></option>
+                            <option value="Mauro Salles Ferreira Leite" label="Mauro Salles Ferreira Leite"></option>
+                            <option value="Mário Donizeti do Nascimento" label="Mário Donizeti do Nascimento"></option>
+							<option value="Matheus Gomes Del Peso Cortez" label="Matheus Gomes Del Peso Cortez"></option>
+							<option value="Murilo Prado de Mello" label="Murilo Prado de Mello"></option>
+							<option value="Omar Augusto Leite Melo" label="Omar Augusto Leite Melo"></option>
+                            <option value="Osvaldo Luiz de Mesquita Gambetti" label="Osvaldo Luiz de Mesquita Gambetti"></option>
+                            <option value="Paulo Sérgio Rosalin Moreno" label="Paulo Sérgio Rosalin Moreno"></option>
+							<option value="Rafael Mercadante Junior" label="Rafael Mercadante Junior"></option>
+                            <option value="Raphael Guilherme Araujo Torrezan" label="Raphael Guilherme Araujo Torrezan"></option>
+							<option value="Regiel Luiz de Mesquita Gambetti" label="Regiel Luiz de Mesquita Gambetti"></option>
+							<option value="Reinaldo Antonio Aleixo" label="Reinaldo Antonio Aleixo"></option>
+                            <option value="Reinaldo Cesar Cafeo" label="Reinaldo Cesar Cafeo"></option>          
+                            <option value="Ricardo Alessi Delfim" label="Ricardo Alessi Delfim"></option>
+                            <option value="Roberta Cristina Paganini de Toledo" label="Roberta Cristina Paganini de Toledo"></option>
+                            <option value="Roberval Modesto da Cunha" label="Roberval Modesto da Cunha"></option>
+							<option value="Rodrigo Luiz Guarnetti" label="Rodrigo Luiz Guarnetti"></option>
+							<option value="Rodrigo Neves de Oliveira Escobar" label="Rodrigo Neves de Oliveira Escobar"></option>
+							<option value="Rodrigo Pieroni Fernandes" label="Rodrigo Pieroni Fernandes"></option>
+                            <option value="Ruth Maria Junqueira de Andrade Pereira e Silva" label="Ruth Maria Junqueira de Andrade Pereira e Silva"></option>
+                            <option value="Salete Aparecida Rossini Lara" label="Salete Aparecida Rossini Lara"></option>
+                            <option value="Silvio Luiz Frank" label="Silvio Luiz Frank"></option>
+							<option value="Tatiana Stroppa" label="Tatiana Stroppa"></option>
+                            <option value="Thiago Munaro Garcia" label="Thiago Munaro Garcia"></option>
+                            <option value="Vanderlei Ferreira de Lima" label="Vanderlei Ferreira de Lima"></option>
+                            <option value="Vania Somaio Teixeira" label="Vania Somaio Teixeira"></option>
+                            <option value="Wagner Aparecido Ismanhoto" label="Wagner Aparecido Ismanhoto"></option>
+							<option value="Wallace Christian Feitosa de Paula" label="Wallace Christian Feitosa de Paula"></option>
+							</optgroup>
+					</select>
+					
+					<br><br>
+					Sala e Bloco:<input type="text" name="sala"> 
+						<select name="Sala">
+							Escolha a sala e o bloco
+							<optgroup label="salas do bloco 2 e 4">
+							<option value="sala 201 bl2" label="sala 201 bl2"></option>
+							<option value="sala 202 bl2" label="sala 202 bl2"></option>
+							<option value="sala 203 bl2" label="sala 203 bl2"></option>
+							<option value="sala 209 bl2" label="sala 209 bl2"></option>
+							<option value="sala 211 bl2" label="sala 211 bl2"></option>
+							<option value="sala 212 bl2" label="sala 212 bl2"></option>
+							<option value="sala 213 bl2" label="sala 213 bl2"></option>
+							<option value="sala 214 bl2" label="sala 214 bl2"></option>
+							<option value="sala 216 bl2" label="sala 216 bl2"></option>
+							<option value="sala 217 bl2" label="sala 217 bl2"></option>
+							<option value="sala 220 bl2" label="sala 220 bl2"></option>
+							<option value="sala 221 bl2" label="sala 221 bl2"></option>
+							<option value="sala 222 bl2" label="sala 222 bl2"></option>
+							<option value="sala 223 bl2" label="sala 223 bl2"></option>
+							<option value="sala 252 bl2" label="sala 252 bl2"></option>
+							<option value="sala 253 bl2" label="sala 253 bl2"></option>
+							<option value="sala 254 bl2" label="sala 254 bl2"></option>
+
+							<optgroup label="salas do bloco 5">
+							<option value="sala 107 bl5" label="sala 107 bl5"></option>
+							<option value="sala 112 bl5" label="sala 112 bl5"></option>
+							<option value="sala 201 bl5" label="sala 201 bl5"></option>
+							<option value="sala 207 bl5" label="sala 207 bl5"></option>
+							<option value="sala 211 bl5" label="sala 211 bl5"></option>
+							</optgroup>
+
+							<optgroup label="escolha qual o laboratorio">
+							<option value="lab01 bl4" label="lab 01 bl4"></option>
+							<option value="lab02 bl4" label="lab 02 bl4"></option>
+							<option value="lab03 bl4" label="lab 03 bl4"></option>
+							<option value="lab04 bl4" label="lab 04 bl2"></option>
+							<option value="lab05 bl4" label="lab 05 bl2"></option>
+							<option value="lab08 bl4" label="lab 08 bl2"></option>
+							</optgroup>
+                    </select>
+					<br><br>
+					Recursos:<br>
+					<input type="checkbox" name="pass">Passador de slide<br>
+					<br>
+					Selecione: 
+					<section class="flex">
+						<div>
+							<input type="checkbox" name="slide">Slide<br>
+							<input type="checkbox" name="bat">Pilha e baterias 9V<br>
+							<input type="checkbox" name="video">Passar video<br>
+							<input type="checkbox" name="grav">Pausar a gravação<br>
+							<input type="checkbox" name="net"> Internet<br>
+						</div>
+						<div>
+							<input type="checkbox" name="canet">Canetão/Giz<br>
+							<input type="checkbox" name="projet">Projetor<br>
+							<input type="checkbox" name="som">Som da sala<br>
+							<input type="checkbox" name="mic">Som do microfone<br>
+							<input type="checkbox" name="comp">Som do computador<br>
+						</div>
+						<div>
+						<br><br><br><br>
+							<input type="submit" name="bot" value="Enviar Pedido de Ajuda">
+						</div>
+					</section>
+				</fieldset>
+				
+
+			
+			<!--<div>
+				<center><img src="logo.png"  width="75%"></center>
+			
+			</div>!-->
+	
+		
+			<?php
+
+				$prof = "";
+				$sala = "";
+				
+			
+				if (empty($_POST['prof'])){
+					if (!empty($_POST['professor'])){
+						$prof = $_POST['professor'];	
+					}
+				}
+				else{
+					$prof = $_POST["prof"];
+			
+				}
+
+				echo "<br>";
+				if (empty($_POST['sala'])){
+					if (!empty($_POST['Sala'])){
+						$sala = $_POST['Sala'];
+					}
+		
+				}
+				else{
+					$sala = $_POST["sala"];
+			
+				}
+
+
+
+				if (!$prof == "" and !$sala == ""){
+					
+				$id = mysql_connect("localhost", "root", "");
+				$con = mysql_select_db("itehelp", $id);
+				$sol = "";
+				
+				$a;
+		
+				$arquivo = 'sol.txt';
+				$handle = fopen( "sol.txt" , 'r' );
+				$ler = fgets($handle);
+				$txt = $ler;
+				fclose($handle);
+			
+			   
+				if(isset($_POST['pass'])){
+					$sol = $sol . "Passador de Slide - ";
+				
+				}
+				if(isset($_POST['slide'])){
+					$sol = $sol . "Passar Slide - ";
+				
+				}
+				if(isset($_POST['bat'])){
+					$sol = $sol . "Bateria - ";
+				
+				}
+				if(isset($_POST['video'])){
+					$sol = $sol . "Passar Vídeo - ";
+				
+				}
+				if(isset($_POST['grav'])){
+					$sol = $sol . "Pausar gravação - ";
+				
+				}
+				if(isset($_POST['net'])){
+					$sol = $sol . "Sem Internet - ";
+				
+				}
+				if(isset($_POST['canet'])){
+					$sol = $sol . "Giz/Canetão - ";
+				
+				}
+				if(isset($_POST['projet'])){
+					$sol = $sol . "Projetor - ";
+				
+				}
+				if(isset($_POST['som'])){
+					$sol = $sol . "Som da Sala - ";
+				
+				}
+				if(isset($_POST['mic'])){
+					$sol = $sol . "Som do Microfone - ";
+				
+				}
+				if(isset($_POST['comp'])){
+					$sol = $sol . "Computador - ";
+				
+				}
+			
+				$bd = $prof . $sala . $sol . "<br>";
+			
+			
+				if ($bd == $txt){
+					
+				}
+				else{
+					$sql3="insert into request (prof, sala, sol)
+					values ('$prof', '$sala', '$sol')";	
+					mysql_query($sql3);
+					mysql_close($id);
+					echo '<script type="text/JavaScript"> 
+					 alert("Enviado com sucesso");
+					</script>';
+				
+					$arquivo = fopen('sol.txt','w+');
+					if ($arquivo) {
+						fwrite($arquivo, $bd);
+						fclose($arquivo);
+					}
+				}
+				
+				}
+		?>
+        
+        
+	<br></br>
+	
+    </body>
+
+</html>
+</html>
---
+++ sobre.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html>
+
+    <head>
+        <meta charsert="utf-8">
+        <meta nome="viemport" content="width=device-width. initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <title>ITE.HElP</title>
+        <link  rel="shortcut icon" href="./logo.png" type="image/x-icon">
+    </head>
+
+
+    <body>
+        <header class="header">
+		   <a href="./index.php">Ajuda</a>
+           
+            <nav>
+                <ul class="menu">
+                    <li><a href="./sobre.php">Sobre</a></li>
+                </ul>
+            </nav>
+        </header>
+		<br>
+		<h1 class="tit">SOBRE</h1>
+
+                <p id="t1">Ite.Help sistema web para auxiliar professores durante as aulas.</p>
+
+    </body>
+
+</html>
